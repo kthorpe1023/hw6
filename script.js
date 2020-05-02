020a12ee139b2344cb8e4ad7c69e65050cc4ac50
Katie Thorpe <kthorpe1023@gmail.com>
2020-05-02 08:41:52 -0600

    styling added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $("button").on("click", searchCity)
+    $("a").on("click", searchCity)
 
     function searchCity(){
         var city = $("input").val();
@@ -13,11 +13,15 @@ $(document).ready(function(){
             url: queryURL,
             mathod: "GET"
         }).then(function(response){
+            var newBtn = $("<button>").text(city);
+            var br = $("<br>");
+            $("#display-past").prepend(newBtn, br);
             console.log(response);
             $("#display-current").empty();
             $("#display-5-day").empty();
             var newDiv = $("<div>")
-            var cityDate = $("<h2>").text(response.city.name + " " + moment().format('MMMM Do YYYY'));
+            var cityDate = $("<h5>").text(response.city.name + "--" + moment().format('MMMM Do YYYY'));
+            var description = $("<p>").text("Forecast: " + response.list[0].weather[0].description);
             var tempF = (response.list[0].main.temp - 273.15) * 1.80 + 32;
             // var tempF1 = $("<span>").text();
             var temp = $("<p>").attr("id", "t").text("Temperature: " + tempF.toFixed(2) + "F");
@@ -25,7 +29,7 @@ $(document).ready(function(){
             var humidity = $("<p>").text("Humidity: " + response.list[0].main.humidity);
             var wind = $("<p>").text("Wind Speed: " + response.list[0].wind.speed);
 
-            newDiv.append(cityDate, temp, humidity, wind);
+            newDiv.append(cityDate, description, temp, humidity, wind);
             $("#display-current").append(newDiv);
             if(tempF <= 40){
                 $("#t").addClass("cold")
@@ -36,17 +40,21 @@ $(document).ready(function(){
             }
 
             var days = 0
+            var heading = $("<h5>").text("5 Day Forcast:")
+            $("#display-5-day").append(heading)
 
             for (var i = 8; i < 40; i = i + 8){
-                var newDate = $("<div>");
-                var nextDay = $("<h3>").text(moment().add(days = days + 1, 'days').format('MMMM Do YYYY'));
+                var newDate = $("<div>").addClass("col s2.75 fiveDay");
+                var nextDay = $("<h6>").text(moment().add(days = days + 1, 'days').format('MMMM Do YYYY'));
+                // var date = $("<p>").text(response.list[i].dt_txt)
                 var iconCode = response.list[i].weather[0].icon;
                 var iconurl = "http://openweathermap.org/img/w/" + iconCode + ".png";
                 var icon = $("<img>").attr("src", iconurl);
+                var description5 = $("<p>").text("Forecast: " + response.list[i].weather[0].description)
                 var tempFa = (response.list[i].main.temp - 273.15) * 1.80 + 32;
                 var temp5 = $("<p>").text("Temperature: " + tempFa.toFixed(2) + "F");
                 var humidity5 = $("<p>").text("Humidity: " + response.list[i].main.humidity)
-                newDate.append(nextDay, icon, temp5, humidity5);
+                newDate.append(nextDay, icon, description5, temp5, humidity5);
                 $("#display-5-day").append(newDate)
             }
 
